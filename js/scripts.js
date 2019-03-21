afda53e729c034efe476dcafd2ac658c065d2ed5
Student <example@example.com>
2019-03-20 19:51:46 -0700

    working JS! with two form groups and 2 different questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
   $("form#trans_survey").submit(function(event){
     event.preventDefault();
-    $("work-answer").show();
+    $("#work-answer").show();
     $("input:checkbox[name=work-transport]:checked").each(function(){
       var TransportMode = $(this).val();
       $('#work-answer').append(TransportMode + "<br>");
+    });
+    $("#fun-answer").show();
+    $("input:checkbox[name=fun-transport]:checked").each(function(){
+      var FunMode = $(this).val();
+      $('#fun-answer').append(FunMode + "<br>");
     });
     $('#trans_survey').hide();
   });
